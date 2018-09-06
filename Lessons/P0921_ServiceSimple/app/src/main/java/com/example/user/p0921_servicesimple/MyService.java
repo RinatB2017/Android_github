5fb6462fec7e57e852b2660b5ba66deb8cff2a6f
RinatB2017 <tux4096@gmail.com>
2018-09-06 21:57:42 +0300

    update

--- Lessons/P0921_ServiceSimple/app/src/main/java/com/example/user/p0921_servicesimple/MyService.java
+++ Lessons/P0921_ServiceSimple/app/src/main/java/com/example/user/p0921_servicesimple/MyService.java
@@ -13,30 +13,30 @@ public class MyService extends Service {
 
     public void onCreate() {
         super.onCreate();
-        Log.d(LOG_TAG, "onCreate");
+        Log.i(LOG_TAG, "onCreate");
     }
 
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(LOG_TAG, "onStartCommand");
+        Log.i(LOG_TAG, "onStartCommand");
         someTask();
         return super.onStartCommand(intent, flags, startId);
     }
 
     public void onDestroy() {
         super.onDestroy();
-        Log.d(LOG_TAG, "onDestroy");
+        Log.i(LOG_TAG, "onDestroy");
     }
 
     public IBinder onBind(Intent intent) {
-        Log.d(LOG_TAG, "onBind");
+        Log.i(LOG_TAG, "onBind");
         return null;
     }
 
     void someTask() {
         new Thread(new Runnable() {
             public void run() {
-                for (int i = 1; i<=10; i++) {
-                    Log.d(LOG_TAG, "i = " + i);
+                for (int i = 1; i<=60; i++) {
+                    Log.i(LOG_TAG, "i = " + i);
                     try {
                         TimeUnit.SECONDS.sleep(1);
                     } catch (InterruptedException e) {
--- Lessons/P0931_ServiceStop/app/src/main/java/com/boss/p0931_servicestop/MyService.java
+++ Lessons/P0931_ServiceStop/app/src/main/java/com/boss/p0931_servicestop/MyService.java
@@ -19,19 +19,19 @@ public class MyService extends Service {
    
   public void onCreate() {
     super.onCreate();
-    Log.d(LOG_TAG, "MyService onCreate");
+    Log.i(LOG_TAG, "MyService onCreate");
     es = Executors.newFixedThreadPool(3);
     someRes = new Object();
   }
    
   public void onDestroy() {
     super.onDestroy();
-    Log.d(LOG_TAG, "MyService onDestroy");
+    Log.i(LOG_TAG, "MyService onDestroy");
     someRes = null;
   }
  
   public int onStartCommand(Intent intent, int flags, int startId) {
-    Log.d(LOG_TAG, "MyService onStartCommand");
+    Log.i(LOG_TAG, "MyService onStartCommand");
     int time = intent.getIntExtra("time", 1);
     MyRun mr = new MyRun(time, startId);
     es.execute(mr);
@@ -50,26 +50,26 @@ public class MyService extends Service {
     public MyRun(int time, int startId) {
       this.time = time;
       this.startId = startId;
-      Log.d(LOG_TAG, "MyRun#" + startId + " create");
+      Log.i(LOG_TAG, "MyRun#" + startId + " create");
     }
      
     public void run() {
-      Log.d(LOG_TAG, "MyRun#" + startId + " start, time = " + time);
+      Log.i(LOG_TAG, "MyRun#" + startId + " start, time = " + time);
       try {
         TimeUnit.SECONDS.sleep(time);
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
       try {
-        Log.d(LOG_TAG, "MyRun#" + startId + " someRes = " + someRes.getClass() );
+        Log.i(LOG_TAG, "MyRun#" + startId + " someRes = " + someRes.getClass() );
       } catch (NullPointerException e) {
-        Log.d(LOG_TAG, "MyRun#" + startId + " error, null pointer");
+        Log.i(LOG_TAG, "MyRun#" + startId + " error, null pointer");
       }
       stop();
     }
 
     void stop() {
-      Log.d(LOG_TAG, "MyRun#" + startId + " end, stopSelfResult("
+      Log.i(LOG_TAG, "MyRun#" + startId + " end, stopSelfResult("
               + startId + ") = " + stopSelfResult(startId));
     }
   }
