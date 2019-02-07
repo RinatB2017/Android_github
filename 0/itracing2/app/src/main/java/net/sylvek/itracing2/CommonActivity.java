97312709af75a0d1540d5d6be7d9e05734244174
RinatB2017 <tux4096@gmail.com>
2019-02-08 02:08:52 +0300

    update

---
+++ 0/itracing2/app/src/main/java/net/sylvek/itracing2/CommonActivity.java
@@ -0,0 +1,36 @@
+package net.sylvek.itracing2;
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sylvek on 28/12/2015.
+ */
+public abstract class CommonActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout.setEnabled(false);
+    }
+
+    protected void setRefreshing(final boolean refreshing)
+    {
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run()
+            {
+                mSwipeRefreshLayout.setRefreshing(refreshing);
+            }
+        });
+    }
+}
