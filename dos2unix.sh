7ccdf17074601f564aca3f140dd3e480868ef89c
RinatB2017 <tux4096@gmail.com>
2019-07-30 21:30:10 +0300

    update

--- dos2unix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -iname "*.xml" -print0 | xargs -0 dos2unix
-find . -iname "*.java" -print0 | xargs -0 dos2unix
-find . -iname "*.bat" -print0 | xargs -0 dos2unix
-find . -iname "*.properties" -print0 | xargs -0 dos2unix
