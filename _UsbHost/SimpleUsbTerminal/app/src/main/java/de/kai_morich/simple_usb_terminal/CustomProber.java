890981375c405867934a8a168b0f35dc141a0130
RinatB2017 <tux4096@gmail.com>
2020-02-01 21:19:31 +0300

    update

--- _UsbHost/SimpleUsbTerminal/app/src/main/java/de/kai_morich/simple_usb_terminal/CustomProber.java
+++ _UsbHost/SimpleUsbTerminal/app/src/main/java/de/kai_morich/simple_usb_terminal/CustomProber.java
@@ -12,6 +12,9 @@ class CustomProber {
     static UsbSerialProber getCustomProber() {
         ProbeTable customTable = new ProbeTable();
         customTable.addProduct(0x16d0, 0x087e, CdcAcmSerialDriver.class); // e.g. Digispark CDC
+        //TODO
+        customTable.addProduct(0x0451, 0xbef3, CdcAcmSerialDriver.class);
+        //---
         return new UsbSerialProber(customTable);
     }
 
